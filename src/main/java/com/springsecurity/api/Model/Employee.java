75f999c4ddad1060185dbcff1a63f06daa375d93
pavankumar063 <pavankumar987321@gmail.com>
2025-03-31 22:57:19 +0530

    Model Class Added

---
+++ src/main/java/com/springsecurity/api/Model/Employee.java
@@ -0,0 +1,48 @@
+package com.springsecurity.api.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String dept;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+    
+    
+    
+    
+	
+
+}
