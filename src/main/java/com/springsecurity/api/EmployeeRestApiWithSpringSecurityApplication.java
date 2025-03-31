50f98d1c4da13d47dc23e6398c6544aa64b037c9
pavankumar063 <pavankumar987321@gmail.com>
2025-03-31 22:47:50 +0530

    first commit

---
+++ src/main/java/com/springsecurity/api/EmployeeRestApiWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestApiWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestApiWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/api/EmployeeRestApiWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestApiWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
